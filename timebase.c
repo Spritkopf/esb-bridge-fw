af931de00cf4b40534a6796f81499037fb420b53
Ricardo Beck <spritkopf@gmail.com>
2020-10-27 21:23:31 +0100

    switched timebase module to nrfx_timer driver
    app_timer has issues working together with ESB mode

--- timebase.c
+++ timebase.c
@@ -2,49 +2,40 @@
 #include "timebase.h"
 
 #include "nrfx_systick.h"
-#include "app_timer.h"
+#include "nrfx_timer.h"
 
-APP_TIMER_DEF(timeout_timer);
+const nrfx_timer_t g_timebase_timer = NRFX_TIMER_INSTANCE(1);
 
-uint8_t g_timeout_flag = 0;
+volatile uint8_t g_timeout_flag = 0;
 
-static void timeout_timer_handler(void* p_context)
+static void timeout_timer_handler(nrf_timer_event_t event_type, void* p_context)
 {
-    g_timeout_flag = 1;
-    app_timer_stop(timeout_timer);
+    if(event_type == NRF_TIMER_EVENT_COMPARE0){
+        g_timeout_flag = 1;
+        nrfx_timer_clear(&g_timebase_timer);
+        nrfx_timer_disable(&g_timebase_timer);
+    }
 }
 void timebase_init(void)
 {
     nrfx_systick_init();
-
-    app_timer_init();
-    app_timer_create(&timeout_timer, APP_TIMER_MODE_SINGLE_SHOT, timeout_timer_handler);
+    nrfx_timer_config_t timer_cfg = NRFX_TIMER_DEFAULT_CONFIG;
+    timer_cfg.frequency = NRF_TIMER_FREQ_1MHz;
+    timer_cfg.mode = NRF_TIMER_MODE_TIMER;
+    timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
+    nrfx_timer_init(&g_timebase_timer, &timer_cfg, timeout_timer_handler);
+    
     
 
-}
-
-uint8_t timebase_timeout_check_values(uint32_t timeout_ms, void* value1, void* value2)
-{
-    uint8_t result = 1;
-    g_timeout_flag = 0;
-    app_timer_start(timeout_timer, APP_TIMER_TICKS(timeout_ms), NULL);
-
-    while(*(uint32_t*)value1 != *(uint32_t*)value2){
-        if(g_timeout_flag == 1){
-            g_timeout_flag = 0;
-            result = 0;
-            break;
-        }
-    }
-
-    app_timer_stop(timeout_timer);
-    return (result);
 }
 
 void timebase_timeout_start(uint32_t timeout_ms)
 {
     g_timeout_flag = 0;
-    app_timer_start(timeout_timer, APP_TIMER_TICKS(timeout_ms), NULL);
+    uint32_t timeout_ticks = nrfx_timer_ms_to_ticks(&g_timebase_timer, timeout_ms);
+    nrfx_timer_extended_compare(&g_timebase_timer, NRF_TIMER_CC_CHANNEL0, timeout_ticks, NRF_TIMER_SHORT_COMPARE0_STOP_MASK, true);
+    nrfx_timer_enable(&g_timebase_timer);
+    
 }
 
 uint8_t timebase_timeout_check(void)
